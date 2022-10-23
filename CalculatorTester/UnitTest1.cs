4f29a397d51f34512811e8042b62f11a730f30d1
hoangtai <tangphuchoangtai@gmail.com.vn>
2022-10-23 15:06:25 +0700

    Git-test

--- CalculatorTester/UnitTest1.cs
+++ CalculatorTester/UnitTest1.cs
@@ -7,14 +7,43 @@ namespace CalculatorTester
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Calculation cal;
+        [TestInitialize]
+        public void SetUp()
         {
-            Calculation cal = new Calculation(10, 5);
-            Assert.AreEqual(15, cal.Execute("+"));
-            Assert.AreEqual(5, cal.Execute("-"));
-            Assert.AreEqual(50, cal.Execute("*"));
-            Assert.AreEqual(2, cal.Execute("/"));
+            this.cal = new Calculation(10, 5);
+        }
+        [
+      TestMethod]
+        public void TestAddOperator()
+        {
+            Assert.AreEqual(cal.Execute("+"), 15);
+        }
+        [
+      TestMethod]
+        public void TestSubOperator()
+        {
+            Assert.AreEqual(cal.Execute("-"), 5);
+        }
+        [
+      TestMethod]
+        public void TestMulOperator()
+        {
+            Assert.AreEqual(cal.Execute("*"), 50);
+        }
+        [
+      TestMethod]
+        public void TestDivOperator()
+        {
+            Assert.AreEqual(cal.Execute("/"), 2);
+        }
+        [
+      TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivByZero()
+        {
+            Calculation c = new Calculation(2, 0);
+            c.Execute("/");
         }
     }
 }
